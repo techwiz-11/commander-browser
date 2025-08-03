afae2b2e0495cd84705cdcc3bfb0aed791312171
techwiz-11 <quatab1479@gmail.com>
2025-08-03 18:18:05 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.getElementById("searchForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const query = document.getElementById("searchBox").value.trim();
-  if (query) {
-    const url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-    window.open(url, "_blank");
-  }
-});
+// Main script.js
